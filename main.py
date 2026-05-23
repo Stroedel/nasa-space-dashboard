1a8026dceb3303843f673e3eab9b4e28bf96bd27
Lars <lars.naert@student.howest.be>
2026-05-23 14:46:34 +0200

    Voeg APOD functionaliteit toe

--- main.py
+++ main.py
@@ -2,6 +2,21 @@ from nasa_client import NasaClient
 from utils import clear_screen, print_header, print_menu, pause
 
 
+def show_apod(client):
+    data = client.get_apod()
+
+    if not data:
+        return
+
+    print("\nAstronomy Picture of the Day")
+    print("-" * 45)
+    print(f"Titel: {data.get('title')}")
+    print(f"Datum: {data.get('date')}")
+    print(f"Type: {data.get('media_type')}")
+    print(f"URL: {data.get('url')}")
+    print(f"\nUitleg:\n{data.get('explanation')}")
+
+
 def main():
     client = NasaClient()
 
@@ -16,12 +31,18 @@ def main():
 
         choice = input("Kies een optie: ")
 
-        if choice == "4":
+        if choice == "1":
+            show_apod(client)
+            pause()
+
+        elif choice == "4":
             print("Programma gestopt.")
             break
 
         else:
-            print("Functionaliteit komt later.")
+            print("Ongeldige keuze.")
             pause()
+
+
 if __name__ == "__main__":
     main()
