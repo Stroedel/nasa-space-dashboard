d93f1adbdf98bdd21fe022a52f85401c1039537a
Lars <lars.naert@student.howest.be>
2026-05-23 12:32:57 +0200

    Laad NASA API key vanuit env bestand

--- nasa_client.py
+++ nasa_client.py
@@ -2,6 +2,9 @@ import os
 import requests
 from dotenv import load_dotenv
 
-class NasaClient:
-    pass
 
+class NasaClient:
+    def __init__(self):
+        load_dotenv()
+        self.api_key = os.getenv("NASA_API_KEY")
+        self.base_url = "https://api.nasa.gov"
