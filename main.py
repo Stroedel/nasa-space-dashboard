211f3aaa47f48d03e6ca33de32c50a8ea4254755
Lars <lars.naert@student.howest.be>
2026-05-23 14:10:53 +0200

    Controleer of NASA API key aanwezig is

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from nasa_client import NasaClient
 from utils import clear_screen, print_header, print_menu, pause
 
+
 def main():
     client = NasaClient()
+
+    if not client.api_key:
+        print("Geen NASA API key gevonden.")
+        return
