6940e7b47e7f675612291891683593db8c27009f
Lars <lars.naert@student.howest.be>
2026-05-23 11:54:50 +0200

    Voeg commandline menu toe

--- utils.py
+++ utils.py
@@ -11,3 +11,10 @@ def print_header():
     print("=" * 45)
     print("        NASA SPACE DASHBOARD 🚀")
     print("=" * 45)
+
+def print_menu():
+    print("1. Astronomy Picture of the Day")
+    print("2. Mars Rover foto's zoeken")
+    print("3. Asteroïden bekijken")
+    print("4. Stoppen")
+    print("-" * 45)
