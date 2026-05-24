7985e4a09dfcd5ee9ab08e7935501fa5ee6a2618
Lars <lars.naert@student.howest.be>
2026-05-24 14:31:32 +0200

    Voeg functie toe om afbeelding te laden

--- gui.py
+++ gui.py
@@ -1,4 +1,8 @@
 import tkinter as tk
+from urllib.request import urlopen
+from io import BytesIO
+
+from PIL import Image, ImageTk
 
 from nasa_client import NasaClient
 
@@ -16,6 +20,26 @@ def clear_result():
 def reset_image_area():
     image_label.pack(pady=10)
     result_text.config(height=8)
+
+
+def show_image_from_url(url):
+    try:
+        reset_image_area()
+
+        image_bytes = urlopen(url).read()
+        data_stream = BytesIO(image_bytes)
+
+        pil_image = Image.open(data_stream)
+        pil_image.thumbnail((480, 360))
+
+        tk_image = ImageTk.PhotoImage(pil_image)
+
+        image_label.config(image=tk_image)
+        image_label.image = tk_image
+
+    except Exception:
+        image_label.config(image="")
+        image_label.image = None
 
 
 root = tk.Tk()
